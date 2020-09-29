466fb197ba6331c5e0147c56e3a29c4fc5a7c454
***ITALO*** <57546102+italo-rodrigues1@users.noreply.github.com>
2020-09-29 19:12:05 -0300

    Initial commit

---
+++ etapas.js
@@ -0,0 +1,160 @@
+let etapas = [
+    {
+        titulo:'VEREADOR',
+        numero:5,
+        candidatos:[
+            {
+                numero:'34341',
+                nome:'Mariano Costa',
+                partido:'TIY',
+                fotos:[
+                    {url:'bigodinho-5.png',legenda:'Vereador'}
+                ]
+            },
+
+            {
+                numero:'55556',
+                nome:'Fabricio Matos',
+                partido:'BRT',
+                fotos:[
+                    {url:'doido-6.png',legenda:'Vereador'}
+                ]
+            },
+
+            {
+                numero:'89541',
+                nome:'Tripa Seca',
+                partido:'YOL',
+                fotos:[
+                    {url:'bigodinho-5.png',legenda:'Vereador'}
+                ]
+            },
+
+            {
+                numero:'66666',
+                nome:'Lula da Silva',
+                partido:'DMO',
+                fotos:[
+                    {url:'doido-6.png',legenda:'Vereador'}
+                ]
+            },
+
+            {
+                numero:'12345',
+                nome:'Dilma Costa',
+                partido:'TIY',
+                fotos:[
+                    {url:'bigodinho-5.png',legenda:'Vereador'}
+                ]
+            },
+
+            {
+                numero:'78897',
+                nome:'Obama Castro',
+                partido:'EUA',
+                fotos:[
+                    {url:'doido-6.png',legenda:'Vereador'}
+                ]
+            },
+
+            {
+                numero:'54678',
+                nome:'Faustao Melo',
+                partido:'GLB',
+                fotos:[
+                    {url:'bigodinho-5.png',legenda:'Vereador'}
+                ]
+            },
+
+            {
+                numero:'32467',
+                nome:'Itamar Junior',
+                partido:'RGH',
+                fotos:[
+                    {url:'doido-6.png',legenda:'Vereador'}
+                ]
+            },
+
+            {
+                numero:'24526',
+                nome:'Vampeta Marcelo',
+                partido:'UMD',
+                fotos:[
+                    {url:'bigodinho-5.png',legenda:'Vereador'}
+                ]
+            },
+
+            {
+                numero:'23678',
+                nome:'Hamilton Paulo',
+                partido:'EHG',
+                fotos:[
+                    {url:'doido-6.png',legenda:'Vereador'}
+                ]
+            },
+
+
+
+        ]
+
+
+    },
+
+    {
+        titulo:'PREFEITO',
+        numero:2,
+        candidatos:[
+            {
+                numero: '10',
+                nome: 'Pablo',
+                partido:'DSD',
+                vice:'Csp',
+                fotos:[
+                    {url:'frota-9.png',legenda:'Prefeito'},
+                    {url:'geraldo-1.png',legenda:'Vice-Prefeito',small:true} 
+                ]
+            },
+
+            {
+                numero: '50',
+                nome: 'Carlos',
+                partido:'UTK',
+                vice:'Ftr',
+                fotos:[
+                    {url:'mamador-2.png',legenda:'Prefeito'},
+                    {url:'mamador-3.png',legenda:'Vice-Prefeito',small:true}
+                ]
+            },
+        ]
+    },
+
+
+    {
+        titulo:'PRESIDENTE',
+        numero:2,
+        candidatos:[
+            {
+                numero: '10',
+                nome: 'Pablo Escobar',
+                partido:'PML',
+                vice:'Rgu',
+                fotos:[
+                    {url:'frota-9.png',legenda:'Prefeito'},
+                    {url:'geraldo-1.png',legenda:'Vice-Prefeito',small:true} 
+                ]
+            },
+
+            {
+                numero: '50',
+                nome: 'Carlos de Andrade',
+                partido:'TCU',
+                vice:'Klr',
+                fotos:[
+                    {url:'mamador-2.png',legenda:'Prefeito'},
+                    {url:'mamador-3.png',legenda:'Vice-Prefeito',small:true}
+                ]
+            },
+        ]
+    }
+
+];
